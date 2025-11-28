5bcfe694e210ae15c2796a6b4de1b89fce57c209
Rayan8992 <rayan.lima3@etec.sp.gov.br>
2025-11-28 15:36:32 -0300

    Update fatorial_test.c

--- fatorial_test.c
+++ fatorial_test.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include "fatorial.h"
 
-int main(void) {
- 
-    assert(fatorial(0) == 1ULL);
-    assert(fatorial(1) == 1ULL);
-    assert(fatorial(2) == 2ULL);
-    assert(fatorial(5) == 120ULL);
-    assert(fatorial(10) == 3628800ULL);
+int main() {
+    assert(fatorial(0) == 1);
+    assert(fatorial(1) == 1);
+    assert(fatorial(2) == 2);
+    assert(fatorial(3) == 6);
+    assert(fatorial(5) == 120);
 
-    assert(fatorial(-1) == 0ULL);
+    assert(fatorial(-1) == 0);
 
     printf("Todos os testes passaram!\n");
     return 0;
