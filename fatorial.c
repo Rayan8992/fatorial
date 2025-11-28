cbce87c0f07a7ed79e7544a78ef52e4eb5b1a1ba
Rayan8992 <rayan.lima3@etec.sp.gov.br>
2025-11-28 15:36:02 -0300

    Update fatorial.c

--- fatorial.c
+++ fatorial.c
@@ -1,13 +1,18 @@
 #include "fatorial.h"
 
 unsigned long long fatorial(int n) {
-    if (n < 0) return 0; // Entrada inválida
-    if (n == 0) return 1ULL;
+    if (n < 0) {
+        return 0;
+    }
 
-    unsigned long long resultado = 1ULL;
+    if (n == 0) {
+        return 1;
+    }
+
+    unsigned long long resultado = 1;
 
     for (int i = 1; i <= n; i++) {
-        resultado *= i;
+        resultado = resultado * i;
     }
 
     return resultado;
